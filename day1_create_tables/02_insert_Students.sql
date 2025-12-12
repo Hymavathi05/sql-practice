f9604868f6ec29501b6249945ec105bca1bb321e
Hymavathi Kotha <kothahymavathi05@gmail.com>
2025-12-12 12:49:23 +0530

    Add Day1 SQL: insert three Student Details
    - 02_insert_Students.sql (INSERT sample rows)

---
+++ day1_create_tables/02_insert_Students.sql
@@ -0,0 +1,12 @@
+
+SQL> INSERT INTO Student VALUES(1,'HYMAVATHI','KOTHA',21,'hyma@gmail.com');
+
+1 row created.
+
+SQL> INSERT INTO Student VALUES(2,'RIYA','SHARMA',22,'riya@gmail.com');
+
+1 row created.
+
+SQL> INSERT INTO Student VALUES(3,'RUPA','ADAPA',20,'rupa@gmail.com');
+
+1 row created.
