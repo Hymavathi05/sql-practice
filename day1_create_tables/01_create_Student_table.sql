6f289557cdc16ee10197356125ad7fc517e9dd49
Hymavathi Kotha <kothahymavathi05@gmail.com>
2025-12-12 12:46:26 +0530

    Add Day1 SQL : create Student table
    Added SQL for Day 1:
    - 01_create_Student_table.sql (CREATE TABLE Student)

---
+++ day1_create_tables/01_create_Student_table.sql
@@ -0,0 +1,9 @@
+SQL> CREATE TABLE Student (
+  2  student_id NUMBER(10),
+  3  student_first_name VARCHAR2(10),
+  4  student_last_name VARCHAR2(10),
+  5  student_age NUMBER(10),
+  6  student_email VARCHAR(50)
+  7  );
+
+Table created.
