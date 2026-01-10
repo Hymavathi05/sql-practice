6613365f639094e4e7321c38f342f9765e668fb6
Hymavathi Kotha <kothahymavathi05@gmail.com>
2026-01-10 22:56:44 +0530

    Refactored Student SQL to Employee-based interview schema
    Updated SQL files to use Employee-based table structure instead of Student.
    Renamed columns to reflect real-world corporate data like EID, EFNAME,ELNAME,ESAL AND EADDRESS.
    This makes the SQL practice aligned with
    Accenture and other MNC interview requirements.

--- day1_create_tables/01_create_Student_table.sql
+++ day1_create_tables/01_create_Student_table.sql
@@ -1,9 +1,9 @@
-SQL> CREATE TABLE Student (
-  2  student_id NUMBER(10),
-  3  student_first_name VARCHAR2(10),
-  4  student_last_name VARCHAR2(10),
-  5  student_age NUMBER(10),
-  6  student_email VARCHAR(50)
+SQL> CREATE TABLE Emplyee (
+  2  EID VARCHAR2(20) PRIMARY KEY,
+  3  EFNAME VARCHAR2(20),
+  4  ELNAME VARCHAR2(20),
+  5  ESAL NUMBER(20),
+  6  EADDRESS VARCHAR(50)
   7  );
 
 Table created.
